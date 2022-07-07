73a1baa123c4a28bcfc54abe71691ddd16f391ec
Andrey Morozov <morozovandrej4758@gmail.com>
2022-07-07 21:17:26 +0300

    Init commit

---
+++ src/main/kotlin/com/ad/adteam/AdTeamApplication.kt
@@ -0,0 +1,11 @@
+package com.ad.adteam
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AdTeamApplication
+
+fun main(args: Array<String>) {
+	runApplication<AdTeamApplication>(*args)
+}
---
+++ src/test/kotlin/com/ad/adteam/AdTeamApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ad.adteam
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AdTeamApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
